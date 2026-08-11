2a6b9004831825cc83dc3a015ccf534edaaee761
Abdullah Zain <abdzain755@gmail.com>
2026-08-11 22:16:30 +0500

    OOPs.py

--- OOPs.py
+++ OOPs.py
@@ -1,3 +1,5 @@
+
+"""OOPs"""
 class calculations:
     def addition(self, num1, num2):
         self.num1=num1
@@ -9,3 +11,38 @@ class calculations:
 obj1=calculations()
 obj1.addition(3463, 23536)
 obj1.add()
+
+
+
+"""Constructor"""
+
+class house:
+    def __init__(self, rooms, fans, ACs):
+        self.num1=rooms
+        self.num2=fans
+        self.num3=ACs
+
+
+    def add(self):
+        print(f"The House will have {self.num1} rooms, {self.num2} fans and {self.num3} ACs")
+
+obj1=house(2,7,1)
+obj1.add()
+
+
+"""Encapsulation"""
+
+class Hospital:
+    def __init__(self,patient,disease):
+        self.patient=patient
+        self.__disease=disease
+
+    def patientdatails(self):
+        print("Patient name is {self.patient} and disease is {self.disease}")   #it wont printthe disease, cuz it's private
+
+    def whatdisease(self):
+        print(self.__disease)    # to know disease
+
+obj1=Hospital("Ali", "Diabetes")
+obj1.patientdetails()
+obj1.whatdisease()
